31ba1cd796650178b11cf6df70410785a66d39e9
truongduyng <truongduyng@gmail.com>
2026-04-11 00:42:59 +0700

    feat: add TinySteps product configuration and associated assets

--- src/products/index.ts
+++ src/products/index.ts
@@ -1,6 +1,7 @@
 import { HONE } from "./hone";
 import { AMFO } from "./amfo";
 import { LICHTA } from "./lichta";
+import { TINYSTEPS } from "./tinysteps";
 import type { ProductConfig } from "@/lib/types";
 
 /**
@@ -8,4 +9,4 @@ import type { ProductConfig } from "@/lib/types";
  * To add a new app: create src/products/{id}/index.ts and import it here.
  * The toolbar product selector appears automatically when length > 1.
  */
-export const PRODUCTS: ProductConfig[] = [HONE, AMFO, LICHTA];
+export const PRODUCTS: ProductConfig[] = [HONE, AMFO, LICHTA, TINYSTEPS];
